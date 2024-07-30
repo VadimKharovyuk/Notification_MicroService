de288416a1bd5f2ef73854d32dc3a6cb4428b9bc
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-07-30 03:46:24 +0300

    +

--- src/main/java/com/example/notification_service/NotificationController.java
+++ src/main/java/com/example/notification_service/NotificationController.java
@@ -1,2 +1,18 @@
-package com.example.notification_service;public class NotificationController {
+package com.example.notification_service;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationController {
+
+
+    @PostMapping("/notifications")
+    public void sendNotification(@RequestBody NotificationRequest request) {
+        // Логика отправки уведомления
+        System.out.println("Sending notification to user: " + request.getUserId());
+        System.out.println("Message: " + request.getMessage());
+    }
+
 }
--- src/main/java/com/example/notification_service/NotificationRequest.java
+++ src/main/java/com/example/notification_service/NotificationRequest.java
@@ -1,2 +1,16 @@
-package com.example.notification_service;public class NotificationRequest {
+package com.example.notification_service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationRequest {
+    private String userId;
+    private String message;
+
 }
