96c2d323e0a850f475e8b8a85d87934018c26b87
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-07-30 15:22:45 +0300

    update H2

--- src/main/java/com/example/notification_service/NotificationController.java
+++ src/main/java/com/example/notification_service/NotificationController.java
@@ -1,18 +1,38 @@
 package com.example.notification_service;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/notifications")
 public class NotificationController {
 
+    private final NotificationRepository notificationRepository;
 
-    @PostMapping("/notifications")
-    public void sendNotification(@RequestBody NotificationRequest request) {
-        // Логика отправки уведомления
-        System.out.println("Sending notification to user: " + request.getUserId());
-        System.out.println("Message: " + request.getMessage());
+    @Autowired
+    public NotificationController(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
     }
 
+    @PostMapping
+    public void sendNotification(@RequestBody NotificationRequest request) {
+        Notification notification = new Notification();
+        notification.setUserId(request.getUserId());
+        notification.setMessage(request.getMessage());
+        notificationRepository.save(notification);
+        System.out.println("Notification saved: " + notification.getMessage());
+    }
+
+    @GetMapping("/{id}")
+    public Notification getNotification(@PathVariable Long id) {
+        System.out.println("Поиск юзера по " + id);
+        return notificationRepository.findById(id).orElse(null);
+    }
+
+    @GetMapping("/all")
+    public List<Notification> getAllNotifications() {
+        return notificationRepository.findAll();
+    }
 }
--- src/main/java/com/example/notification_service/NotificationRequest.java
+++ src/main/java/com/example/notification_service/NotificationRequest.java
@@ -5,12 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Setter
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class NotificationRequest {
     private String userId;
     private String message;
-
 }
